b9fbf67f79e6a3797855f6854723d7aa2eae6f8a
Jayson R Lynch <jaysonl@mit.edu>
2014-11-26 15:39:38 -0500

    more analysis

--- MITTunnels/analysis/wifi_data_analysis.py
+++ MITTunnels/analysis/wifi_data_analysis.py
@@ -28,11 +28,19 @@ colors = []
 color_count = 0 
 
 closest_list = []
+
+closest_average_list = []
+acount = -1
+"""
 for a, i in locations.items():
     color_count += 1
     closest = a
     closest_dist = 10**10
     colsest_loc_dsit = 10**10
+    matches = 0
+    x_ave = 0
+    y_ave = 0
+
     for b, j in locations.items():
         if i != j:
             loc_dist = ((float(i[0]['x']) - float(j[0]['x']))**2 + (float(i[0]['y']) - float(j[0]['y']))**2)**.5
@@ -60,7 +68,41 @@ for a, i in locations.items():
 		closest_loc_dist = loc_dist
 #	    colors.append(color_list[color_count-1])
     closest_list.append([[i[0]['x'], i[0]['y']],[closest[0]['x'], closest[0]['y']], closest_dist, closest_loc_dist])
-print(closest_list)
+
+    for b, j in locations.items():
+        if i != j:
+            dist_sum = 0
+            for k in i:
+               ##print(k)
+                current_dist = float(k['LEVEL'])
+                for l in j:
+                    if k['MAC'] == l['MAC']:
+                        current_dist -=  float(l['LEVEL'])
+                dist_sum += current_dist**2
+            for k in j:
+               ##print(k)
+                current_dist = float(k['LEVEL'])
+                for l in i:
+                    if k['MAC'] == l['MAC']:
+                        current_dist = 0
+                dist_sum += current_dist**2
+
+            distances_loc.append(loc_dist)
+            distances_signal.append(dist_sum**.5)
+            if dist_sum < closest_dist:
+                closest = j
+                closest_dist = dist_sum
+                closest_loc_dist = loc_dist
+#           colors.append(color_list[color_count-1])
+            if dist_sum < closest_list[acount][2] + 100:
+                matches += 1
+                x_ave += float(j[0]['x'])
+                y_ave += float(j[0]['y'])
+
+
+    closest_average_list.append([[i[0]['x'], i[0]['y']],[x_ave/matches, y_ave/matches]])
+"""
+
 
 #plt.figure(1)           
 #plt.subplot(111)
@@ -68,11 +110,16 @@ print(closest_list)
 #plt.title('Signal Distances Using Euclidian Metric')
 #plt.ylabel('Signal Euclidian Distance')
 #plt.xlabel('Physical Distance')
-"""
-perfect_match = []
 
+perfect_match = []
+closest_average_list = []
+acount = -1
 for a, i in locations.items():
+    acount += 1
     color_count += 1
+    closest = a
+    closest_dist = 10**10
+    colsest_loc_dsit = 10**10
     for b, j in locations.items():
         if i != j:
             
@@ -105,12 +152,58 @@ for a, i in locations.items():
             if dist_sum == 0:
 		perfect_match.append([a,b])
 	    distances_loc.append(loc_dist)
-            distances_signal.append(dist_sum)
+            if dist_sum < closest_dist:
+                closest = j
+                closest_dist = dist_sum
+                closest_loc_dist = loc_dist
+    closest_list.append([[i[0]['x'], i[0]['y']],[closest[0]['x'], closest[0]['y']], closest_dist, closest_loc_dist])
+    matches = 0
+    x_ave = 0
+    y_ave = 0
+    for b, j in locations.items():
+        if i != j:
+
+            loc_dist = ((float(i[0]['x']) - float(j[0]['x']))**2 + (float(i[0]['y']) - float(j[0]['y']))**2)**.5
+            dist_sum = 0
+            for k in i:
+                if k['LEVEL'] == '0':
+                    current_dist = 0
+                else:
+                    current_dist = 1
+                for l in j:
+                    if k['MAC'] == l['MAC']:
+                        if l['LEVEL'] == '0':
+                            current_dist += 0
+                        else:
+                            current_dist -= 1
+                dist_sum += abs(current_dist)
+            for k in j:
+                if k['LEVEL'] == '0':
+                    current_dist = 0
+                else:
+                    current_dist = 1
+                for l in i:
+                    if k['MAC'] == l['MAC']:
+                        if l['LEVEL'] == '0':
+                            current_dist += 0
+                        else:
+                            current_dist -= 1
+                dist_sum += abs(current_dist)
+            if dist_sum == 0:
+                perfect_match.append([a,b])
+            distances_loc.append(loc_dist)
+            if dist_sum == closest_list[acount][2]:
+		matches += 1
+		x_ave += float(j[0]['x'])
+		y_ave += float(j[0]['y'])
+		
+    closest_average_list.append([[i[0]['x'], i[0]['y']],[x_ave/matches, y_ave/matches]])
+#           colors.append(color_list[color_count-1])
 #            colors.append(color_list[color_count-1])
 #print(perfect_match)
 print(len(perfect_match))
 print(len(locations))
-"""
+
 #plt.scatter(distances_loc, distances_signal)
 #plt.title('Signal Distances Using Bit Vector Metric')
 #plt.ylabel('Signal Distance in # APs')
@@ -140,7 +233,7 @@ print(len(locations))
 #print(distances)
 
 fig, ax = plt.subplots()
-for i in closest_list:
+for i in closest_average_list:
 	ax.add_line(Line2D([i[0][0], i[1][0]], [i[0][1], i[1][1]]))
 plt.title('pairs of closest points')
 plt.ylabel('y coordinate')
